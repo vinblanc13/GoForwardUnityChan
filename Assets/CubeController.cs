fd168b87e3f7f649c0049e54be20251a44f846c8
megmin <phoenix.raid.0906@gmail.com>
2019-09-02 09:01:29 +0900

    GoForwardUnityChanのコミット
    Lesson7発展課題まで対応済み

---
+++ Assets/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Cube")
+        {
+            audioSource.Play();
+        }
+    }
+}
